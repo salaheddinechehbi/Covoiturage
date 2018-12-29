1eef6eeb66a57f9df699ee045a2b2dcdd451ca92
imadeddine <imadeddine@192.168.1.16>
2018-12-29 20:06:54 +0100

    payeBeans

--- src/java/beans/Paye.java
+++ src/java/beans/Paye.java
@@ -1,6 +1,51 @@
 
 package beans;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Paye {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String Nom;
+
+    public Paye() {
+    }
+
+    public Paye(String Nom) {
+        this.Nom = Nom;
+    }
+
+    public Paye(int id, String Nom) {
+        this.id = id;
+        this.Nom = Nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return Nom;
+    }
+
+    public void setNom(String Nom) {
+        this.Nom = Nom;
+    }
+
+    @Override
+    public String toString() {
+        return "Paye{" + "id=" + id + ", Nom=" + Nom + '}';
+    }
+    
+    
+    
     
 }
