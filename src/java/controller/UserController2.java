16d9d5a16603c7b29afc9717a24050931ce84d99
imadeddine <imadeddine@192.168.1.11>
2019-01-21 18:27:50 +0100

    UserController2

---
+++ src/java/controller/UserController2.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import beans.User;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import service.UserService;
+
+/**
+ *
+ * @author imadeddine
+ */
+@WebServlet(name = "UserController2", urlPatterns = {"/UserController2"})
+public class UserController2 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/Json;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            String nom=request.getParameter("nom");
+            String email=request.getParameter("email");
+            String phone=request.getParameter("phone");
+            String cle=request.getParameter("cle");
+            String pass=request.getParameter("pass");
+            String active = request.getParameter("active");
+            String profile=request.getParameter("profile");
+            UserService us = new UserService();
+            Gson js = new Gson();
+            
+            us.create(new User(nom, email, cle, pass, cle, cle, Integer.parseInt(active), profile));
+           
+            out.write(js.toJson(us.findAll()));
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
